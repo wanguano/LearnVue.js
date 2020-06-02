520a99154b6470cae6f43326835a2dada3bc2f9f
wanguan <1326075334@qq.com>
2020-06-02 17:09:32 +0800

    d06.02

---
+++ 01-02-03-04day/作业/15-前端模块化/module1.mjs
@@ -0,0 +1,5 @@
+export const a = 10;
+
+export default function sum(a=1,b=2) {
+  return a+b
+}
---
+++ 01-02-03-04day/作业/15-前端模块化/module2.mjs
@@ -0,0 +1,5 @@
+import { a } from "./module1.mjs";
+console.log(a);
+
+import sum from './module1.mjs'
+console.log(sum(5,5));
