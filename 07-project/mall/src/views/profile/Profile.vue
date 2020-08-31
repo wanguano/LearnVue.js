d9cdb77306f9b4f86340a3e18a2958b193a9eb6a
wanguan <1326075334@qq.com>
2020-08-31 15:56:37 +0800

    update

--- 07-project/mall/src/views/profile/Profile.vue
+++ 07-project/mall/src/views/profile/Profile.vue
@@ -4,16 +4,19 @@
     <nav-bar class="profile-nav">
       <template #center>我的档案</template>
     </nav-bar>
-
+    <!-- ProfileHeader 头部信息 -->
+    <profile-header/> 
   </div>
 </template>
 
 <script>
 import NavBar from 'components/common/navbar/NavBar'
+import ProfileHeader from './childComps/Profile-header'
 export default {
   name: "",
   components: {
-    NavBar
+    NavBar,
+    ProfileHeader,
   }
 }
 </script>
